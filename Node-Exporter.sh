5c61a845acedeadf248768e511b413f39811c898
subhrajitsahoo72-in <subhrajitsahoo72@gmail.com>
2026-08-21 10:18:58 +0530

    Update Node-Exporter.sh

--- Node-Exporter.sh
+++ Node-Exporter.sh
@@ -4,3 +4,27 @@ tar -xvf node_exporter-1.6.1.linux-amd64.tar.gz
 sudo mv node_exporter-1.6.1.linux-amd64/node_exporter /usr/local/bin/
 rm -rf node_exporter*
 node_exporter --version
+
+# fter that used the cmmmand 
+in this path 
+
+sudo vi /etc/systemd/system/node_exporter.service
+
+put 
+
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+StartLimitIntervalSec=500
+StartLimitBurst=5
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+Restart=on-failure
+RestartSec=5s
+ExecStart=/usr/local/bin/node_exporter \
+    --collector.logind
+[Install]
+WantedBy=multi-user.target
