97e3533ba2d1ca754a8932119f5fac3a61cdab02
subhrajitsahoo72-in <subhrajitsahoo72@gmail.com>
2026-08-21 10:15:35 +0530

    Add Node Exporter installation script

---
+++ Node-Exporter.sh
@@ -0,0 +1,6 @@
+sudo useradd --system --no-create-home --shell /bin/false node_exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz
+tar -xvf node_exporter-1.6.1.linux-amd64.tar.gz
+sudo mv node_exporter-1.6.1.linux-amd64/node_exporter /usr/local/bin/
+rm -rf node_exporter*
+node_exporter --version
